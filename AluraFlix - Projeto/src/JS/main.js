e5a252847d9214b62110dbf95fa3d468d0854dbf
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2023-02-02 19:22:17 -0300

    imagens add

---
+++ AluraFlix - Projeto/src/JS/main.js
@@ -0,0 +1,22 @@
+
+var listaFilmes = []
+listaFilmes[0] = 'src/imagens/01.jpg';
+listaFilmes[1] = 'src/imagens/02.jpg';
+listaFilmes[2] = 'src/imagens/03.jpg';
+listaFilmes[3] = 'src/imagens/04.jpg';
+listaFilmes[4] = 'src/imagens/05.jpg';
+listaFilmes[5] = 'src/imagens/06.jpg';
+listaFilmes[6] = 'src/imagens/07.jpg';
+
+// for (let i = 0; i < listaFilmes.length; i++) {
+//     const element = array[listaFilmes];
+    
+// }
+
+document.write('<img src= ' + listaFilmes[0] + '>');
+document.write('<img src= ' + listaFilmes[1] + '>');
+document.write('<img src= ' + listaFilmes[2] + '>');
+document.write('<img src= ' + listaFilmes[3] + '>');
+document.write('<img src= ' + listaFilmes[4] + '>');
+document.write('<img src= ' + listaFilmes[5] + '>');
+document.write('<img src= ' + listaFilmes[6] + '>');
